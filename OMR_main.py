2194d56f470daa1cf3f7000c3c8253192520fb82
brunnopetty <121527052+brunnopetty@users.noreply.github.com>
2023-11-16 11:29:31 -0300

    git add

--- OMR_main.py
+++ OMR_main.py
@@ -10,95 +10,131 @@ heightImg = 700
 questoes = 20
 escolhas = 5
 respostas = [1,2,0,3,4,2,1,4,3,3,2,0,1,1,1,1,2,0,0,4]
+webcamFeed = True
+cameraNo = 1
 #################################
 
+cap = cv2.VideoCapture(cameraNo)
+cap.set(10,150)
 
-img = cv2.imread(path)
+while True:
+    if webcamFeed: sucess, img = cap.read()
+    else: img = cv2.imread(path)
 
-# PRE PROCESSAMENTO
-img = cv2.resize(img,(widthImg,heightImg))
-imgContours = img.copy()
-imgBiggestContours = img.copy()
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-imgCanny = cv2.Canny(imgBlur,10,50)
+    # PRE PROCESSAMENTO
+    img = cv2.resize(img,(widthImg,heightImg))
+    imgContours = img.copy()
+    imgFinal = img.copy()
+    imgBiggestContours = img.copy()
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,10,50)
 
-# PROCURANDO TODOS CONTORNOS
-contours, hierarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(imgContours,contours, -1,(0,255,0),10)
-# PROCURANDO RETANGULOS
-rectCon= utlis.rectContour(contours)
-biggestContour = utlis.getCornerPoints(rectCon[0])
-gradePoints = utlis.getCornerPoints(rectCon[1])
-#print(biggestContour)
+    try:
 
-if biggestContour.size !=0 and gradePoints.size !=0:
-    cv2.drawContours(imgBiggestContours,biggestContour,-1,(0,255,0),20)
-    cv2.drawContours(imgBiggestContours,gradePoints,-1,(255, 0, 0),20)
+        # PROCURANDO TODOS CONTORNOS
+        contours, hierarchy = cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(imgContours,contours, -1,(0,255,0),10)
+        # PROCURANDO RETANGULOS
+        rectCon= utlis.rectContour(contours)
+        biggestContour = utlis.getCornerPoints(rectCon[0])
+        gradePoints = utlis.getCornerPoints(rectCon[1])
+        #print(biggestContour)
 
-    biggestContour=utlis.reorder(biggestContour)
-    gradePoints = utlis.reorder(gradePoints)
+        if biggestContour.size !=0 and gradePoints.size !=0:
+            cv2.drawContours(imgBiggestContours,biggestContour,-1,(0,255,0),20)
+            cv2.drawContours(imgBiggestContours,gradePoints,-1,(255, 0, 0),20)
 
-    pt1 = np.float32(biggestContour)
-    pt2 = np.float32([[0,0],[widthImg, 0], [0, heightImg],[widthImg,heightImg]])
-    matrix = cv2.getPerspectiveTransform(pt1,pt2)
-    imgWarpColored= cv2.warpPerspective(img, matrix,(widthImg, heightImg))
+            biggestContour=utlis.reorder(biggestContour)
+            gradePoints = utlis.reorder(gradePoints)
 
-    ptG1 = np.float32(gradePoints)
-    ptG2 = np.float32([[0, 0], [325, 0], [0, 150], [325, 150]])
-    matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
-    imgGradeDisplay = cv2.warpPerspective(img, matrixG, (325, 150))
-    #cv2.imshow("Grade", imgGradeDisplay)
+            pt1 = np.float32(biggestContour)
+            pt2 = np.float32([[0,0],[widthImg, 0], [0, heightImg],[widthImg,heightImg]])
+            matrix = cv2.getPerspectiveTransform(pt1,pt2)
+            imgWarpColored= cv2.warpPerspective(img, matrix,(widthImg, heightImg))
 
-    #APLICAR LIMITE
-    imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
-    imgTresh = cv2.threshold(imgWarpGray,140,255,cv2.THRESH_BINARY_INV) [1]
+            ptG1 = np.float32(gradePoints)
+            ptG2 = np.float32([[0, 0], [325, 0], [0, 150], [325, 150]])
+            matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
+            imgGradeDisplay = cv2.warpPerspective(img, matrixG, (325, 150))
+            #cv2.imshow("Grade", imgGradeDisplay)
 
-    boxes = utlis.splitBoxes(imgTresh)
-    #cv2.imshow("Teste", boxes[2])
-    print(cv2.countNonZero(boxes[1]),cv2.countNonZero(boxes[2]))
+            #APLICAR LIMITE
+            imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+            imgTresh = cv2.threshold(imgWarpGray,140,255,cv2.THRESH_BINARY_INV) [1]
 
-#OBTER VALORES DE PIXEL DIFERENTE DE ZERO
-    myPixelVal = np.zeros((questoes,escolhas))
-    countC= 0
-    countR= 0
+            boxes = utlis.splitBoxes(imgTresh)
+            #cv2.imshow("Teste", boxes[2])
+            print(cv2.countNonZero(boxes[1]),cv2.countNonZero(boxes[2]))
 
-    for image in boxes:
-        totalPixels = cv2.countNonZero(image)
-        myPixelVal[countR][countC] = totalPixels
-        countC +=1
-        if (countC == escolhas):countR +=1 ;countC=0
-    #print(myPixelVal)
+        #OBTER VALORES DE PIXEL DIFERENTE DE ZERO
+            myPixelVal = np.zeros((questoes,escolhas))
+            countC= 0
+            countR= 0
 
-#MOSTRANDO OS VALORES DAS MARCAÇÕES
-    myIndex = []
-    for x in range (0, questoes):
-        arr = myPixelVal[x]
-        print("arr", arr)
-        myIndexVal = np.where(arr==np.amax(arr))
-        #print(myIndexVal[0])
-        myIndex.append(myIndexVal[0][0])
-    #print(myIndex)
+            for image in boxes:
+                totalPixels = cv2.countNonZero(image)
+                myPixelVal[countR][countC] = totalPixels
+                countC +=1
+                if (countC == escolhas):countR +=1 ;countC=0
+            #print(myPixelVal)
 
-    #NOTAS
-    grading=[]
-    for x in range(0,questoes):
-        if respostas[x] == myIndex[x]:
-            grading.append(1)
-        else: grading.append(0)
-    #print(grading)
-    nota = (sum(grading)/questoes) *100 #NOTA FINAL
-    print(nota)
+        #MOSTRANDO OS VALORES DAS MARCAÇÕES
+            myIndex = []
+            for x in range (0, questoes):
+                arr = myPixelVal[x]
+                print("arr", arr)
+                myIndexVal = np.where(arr==np.amax(arr))
+                #print(myIndexVal[0])
+                myIndex.append(myIndexVal[0][0])
+            #print(myIndex)
 
-    #MOSTRAR RESPOSTAS
-    imgResult = imgWarpColored.copy()
-    imgResult = utlis.showAnswers(imgResult, myIndex, grading, respostas, questoes, escolhas)
+            #NOTAS
+            grading=[]
+            for x in range(0,questoes):
+                if respostas[x] == myIndex[x]:
+                    grading.append(1)
+                else: grading.append(0)
+            #print(grading)
+            nota = (sum(grading)/questoes) *100 #NOTA FINAL
+            print(nota)
 
-    imgBlank = np.zeros_like(img)
-    imageArray = ([img,imgGray,imgBlur,imgCanny],
-            [imgContours,imgBiggestContours,imgWarpColored,imgTresh],
-            [imgResult,imgBlank,imgBlank,imgBlank])
-    imgStacked = utlis.stackImages(imageArray,0.3)
+            #MOSTRAR RESPOSTAS
+            imgResult = imgWarpColored.copy()
+            imgResult = utlis.showAnswers(imgResult, myIndex, grading, respostas, questoes, escolhas)
+            imRawDrawing = np.zeros_like(imgWarpColored)
+            imRawDrawing = utlis.showAnswers(imRawDrawing, myIndex, grading, respostas, questoes, escolhas)
+            invMatrix = cv2.getPerspectiveTransform(pt2, pt1)
+            imgInvWarp = cv2.warpPerspective(imRawDrawing, invMatrix, (widthImg, heightImg))
 
-cv2.imshow("Stacked Images",imgStacked)
-cv2.waitKey(0)
+            imgRawGrade = np.zeros_like(imgGradeDisplay)
+            cv2.putText(imgRawGrade,str(int(nota))+"%",(80,100),cv2.FONT_HERSHEY_COMPLEX,3,(0,255,255),4)
+            invMatrixG = cv2.getPerspectiveTransform(ptG2, ptG1)
+            imgInvGradeDisplay = cv2.warpPerspective(imgRawGrade, invMatrixG, (widthImg, heightImg))
+
+            imgFinal = cv2.addWeighted(imgFinal, 1, imgInvWarp, 1, 0)
+            imgFinal = cv2.addWeighted(imgFinal, 1, imgInvGradeDisplay, 1, 0)
+
+
+            imgBlank = np.zeros_like(img)
+            imageArray = ([img,imgGray,imgBlur,imgCanny],
+                    [imgContours,imgBiggestContours,imgWarpColored,imgTresh],
+                    [imgResult,imRawDrawing,imgInvWarp,imgFinal])
+
+    except:
+        imgBlank = np.zeros_like(img)
+        imageArray = ([img, imgGray, imgBlur, imgCanny],
+                      [imgBlank, imgBlank, imgBlank, imgBlank],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
+
+    lables = [["Original", "Gray", "Blur", "Canny"],
+                ["Contours", "Biggest Con", "Warp", "Treshold"],
+                ["Result", "Raw Drawing", "Inv Warp", "Final"]]
+
+    imgStacked = utlis.stackImages(imageArray,0.3,lables)
+
+    cv2.imshow("Resultado Final", imgFinal)
+    cv2.imshow("Imagens Empilhadas",imgStacked)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("ResultadoFinal.jpg", imgFinal)
+        cv2.waitKey(300)
